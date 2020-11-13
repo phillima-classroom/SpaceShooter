4cfe28fac6060ba6bb71379c73306779a4e0eb21
phillima <phyllipe_slf@yahoo.com.br>
2020-11-13 13:23:01 -0300

    ArmaPadrao Triplo

---
+++ SpaceShooter/Assets/Scripts/arma/ArmaPadraoTriplo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.arma
+{
+    public class ArmaPadraoTriplo : Arma
+    {
+
+        private void Awake() {
+            init(0.5f);
+        }
+
+        public override void atirar() {
+            if (Time.time > NextFire) {
+                NextFire = fireRate + Time.time;
+                Vector3 posUpper = GameObject.FindGameObjectWithTag("UpperShooter").transform.position;
+                Vector3 posLower = GameObject.FindGameObjectWithTag("LowerShooter").transform.position;
+                Vector3 posCenter = GameObject.FindGameObjectWithTag("Ponta_Nave").transform.position;
+                Instantiate(municao, posUpper, Quaternion.identity);
+                Instantiate(municao, posLower, Quaternion.identity);
+                Instantiate(municao, posCenter, Quaternion.identity);
+                audioSource.PlayOneShot(audioSource.clip);
+            }
+        }
+    }
+}
