0a94a76399445da70ecd5f1b3cff4940ddc9a2e0
phillima <phyllipe_slf@yahoo.com.br>
2020-12-13 11:15:27 -0300

    Simples Tests

---
+++ SpaceShooter/Assets/Editor/ExampleTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class ExampleTest
+{
+    [Test]
+    public void passingTest() {
+        Assert.Pass();
+    }
+
+}
---
+++ SpaceShooter/Assets/Editor/ExampleTest2.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ExampleTest2
+{
+
+    [UnityTest]
+    public IEnumerator testeNaveExists() {
+        EditorSceneManager.OpenScene("Assets/Scenes/Level1.unity");
+
+        yield return null;
+        Assert.IsTrue(GameObject.FindObjectOfType<Nave>());
+    }
+
+}
